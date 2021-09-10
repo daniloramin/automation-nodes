368ef10b0ec5d24db6a78188669c460b3ad9c3f8
daniloramin <daniloramin00@hotmail.com>
2021-09-10 17:13:06 -0300

    Finalizado Correios Service

--- src/correios/correios.controller.ts
+++ src/correios/correios.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Get, Res } from '@nestjs/common';
+import { CorreiosService } from './correios.service';
+import { Response } from 'express';
+
 
 @Controller('correios')
-export class CorreiosController {}
+export class CorreiosController {
+    constructor(private correiosService: CorreiosService){}
+
+    @Get(":codigo")
+    async rastreio(@Param("codigo") codigo: string, @Res() res: Response) {
+        
+        try {
+            const eventos = await this.correiosService.rastreio(codigo);
+            res.status(200).json({ success: true, data: eventos });
+
+        } catch (error) {
+         res.status(400).json({ success: false, message: error.message });
+         
+        }
+
+    }
+}
--- src/correios/correios.service.ts
+++ src/correios/correios.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class CorreiosService {}
+export class CorreiosService {
+
+    async rastreio(codigo: string): Promise<any> {
+        const rastreioBody = {
+            "code": codigo, 
+            "type": "LS"
+        }
+
+        const { data } = await axios.post(`https://correios.contrateumdev.com.br/api/rastreio`, rastreioBody);
+
+        //const rastreio = data.objeto;
+
+        // console.log(rastreio.evento.map((e) => console.log(e)));
+        // console.log(data.objeto[0])
+
+        const eventos = [];
+
+        data.objeto[0].evento.map((e) => {
+            const { data, hora, descricao, unidade: { cidade, tipounidade } } = e
+            
+            eventos.push({ data, hora, descricao, cidade, tipounidade });
+        });
+
+        return eventos;
+    }
+}
